81a736fd9b946f8842ae277b5161c19ca7521555
Pascal Hartig <passy@twitter.com>
2014-09-03 17:28:39 +0200

    Initial commit

---
+++ Main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import FRP.Yampa
+import Control.Concurrent
+
+type Pos = Double
+type Vel = Double
+
+fallingBall :: Pos -> SF () (Pos, Vel)
+fallingBall y0 = (constant (-9.81) >>> integral) >>> ((integral >>^ (+ y0)) &&& identity)
+
+main :: IO ()
+main =
+    reactimate (return ())
+               (\ _ -> threadDelay 100000 >> return (0.1, Nothing))
+               (\ _ (pos, vel) -> putStrLn ("pos: " ++ show pos ++ ", vel: " ++ show vel) >> return False)
+               (fallingBall 10.0)
