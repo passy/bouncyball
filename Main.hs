8fcdd4ebc540305969dcb5ab267bafff39edd4a4
Pascal Hartig <passy@twitter.com>
2014-09-03 19:12:13 +0200

    It's actually bouncing. OMG

--- Main.hs
+++ Main.hs
@@ -11,13 +11,24 @@ import Types
 
 fallingBall :: Pos -> Vel -> SF () (Pos, Vel)
 fallingBall y0 v0 =
+    -- First time I'm using the Arrow syntax. Feels weird.
     proc _ -> do
         v <- integral >>^ (+ v0) -< (-9.81)
         y <- integral >>^ (+ y0) -< v
         returnA -< (y, v)
 
+bouncingBall :: Pos -> Vel -> SF () (Pos, Vel)
+bouncingBall y0 v0 = switch (bb y0 v0) (\ (pos, vel) -> bouncingBall pos (-vel))
+    where
+        bb y0' v0' = proc input -> do
+            (pos, vel) <- fallingBall y0' v0' -< input
+            -- If the condition input is true, we fire an event which is
+            -- caught by the switch above.
+            event' <- edge -< pos <= 0
+            returnA -< ((pos, vel), event' `tag` (pos, vel))
+
 mainSF :: SF () (IO ())
-mainSF = (fallingBall 10.0 0.0) >>^ \ (pos, vel) -> putStrLn ("pos: " ++ show pos ++ ", vel: " ++ show vel) >> draw pos
+mainSF = (bouncingBall 10.0 0.0) >>^ \ (pos, vel) -> putStrLn ("pos: " ++ show pos ++ ", vel: " ++ show vel) >> draw pos
 
 -- | Main, initializes Yampa and sets up reactimation loop
 main :: IO ()
